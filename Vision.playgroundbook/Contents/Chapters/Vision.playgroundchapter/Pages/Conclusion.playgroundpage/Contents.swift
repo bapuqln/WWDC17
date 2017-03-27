1bdc9692792aa74d3ba5f2100acaa6d160d4e902
Github <shusain93>
2017-03-26 19:49:13 -0600

    Added Licensing for CC

--- Vision.playgroundbook/Contents/Chapters/Vision.playgroundchapter/Pages/Conclusion.playgroundpage/Contents.swift
+++ Vision.playgroundbook/Contents/Chapters/Vision.playgroundchapter/Pages/Conclusion.playgroundpage/Contents.swift
@@ -10,4 +10,23 @@
  ### “It’s the right thing to do”
  
  The iOS Accessibility Guidelines suggests deep integration because when making software have a moral obligation to *do it right*. While you may not know anyone who uses a screenreader or other low vision tools, it’s vital to ensure that your apps are not just beautifully designed but beautifully designed for all. Accessibility has never been a lucrative investment. It’s often thankless: the vast majority of people don’t rush to download an update because it has greater accessibility support. Most users wouldn’t even know how to access such features. Accessibility is an investment in our world community. Sure, most people will never it’s there but it will undoubtedly make someone’s life much easier — and isn’t that what technology is for, anyways?
+
+ ---
+ 
+ &nbsp;
+
+ &nbsp;
+
+ &nbsp;
+
+ &nbsp;
+
+ Licenses:
+ 
+ [Nature Icon Pack by Freepik.com licensed under](http://www.flaticon.com/packs/spring-7) [Creative Commons BY 3.0 License](https://creativecommons.org/licenses/by/3.0/us/legalcode)
+ 
+ [Sun Icon by Madebyoliver licensed](http://www.flaticon.com/packs/weather-set-2) [Creative Commons BY 3.0 License](https://creativecommons.org/licenses/by/3.0/us/legalcode)
+
+ [Eye Icon by Madebyoliver licensed](http://www.flaticon.com/packs/essential-collection) [Creative Commons BY 3.0 License](https://creativecommons.org/licenses/by/3.0/us/legalcode)
+
  */
